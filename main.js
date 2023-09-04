e3a12aa89ab91843cb0ca20181d5d55041151c0c
reducesteps <jonathanoakey0@gmail.com>
2023-09-04 23:26:13 +0000

    Updated UI and started game logic

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 // Main JavaScript file for Blackjack Game
 
-// Initialize game variables here
+// Initialize game variables
+const playerCards = document.getElementById('player-cards');
+const dealerCards = document.getElementById('dealer-cards');
+const gameStatus = document.getElementById('game-status');
+
+// Initialize deck and shuffle
+let deck = [];
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+
+function shuffleDeck() {
+  deck = [];
+  for (let suit of suits) {
+    for (let value of values) {
+      deck.push({ suit, value });
+    }
+  }
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+}
 
 // Add game logic here
